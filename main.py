ae17a9e808330a3b5446ab2d131aad3a3477d4b6
Zulfikar Alom <alomm@compsci.cs.umanitoba.ca>
2024-02-26 21:04:23 -0600

    Update Comment

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ from deeprobust.graph.data import Dataset, Dpr2Pyg
 from model.GIN import GIN
 from model.GSAGE import GraphSAGE
 from deeprobust.graph.targeted_attack import Nettack
+from deeprobust.graph.targeted_attack import RND
+from deeprobust.graph.targeted_attack import FGA
+from deeprobust.graph.targeted_attack import SGAttack
+from deeprobust.graph.targeted_attack import IGAttack
+from deeprobust.graph.defense import SGC
 import scipy.sparse as sp
 
 """
@@ -115,26 +120,95 @@ def set_up_surrogate_model(features, adj, labels, idx_train, idx_val):
     surrogate.fit(features, adj, labels, idx_train, idx_val, patience=30)
     return surrogate
 
-def attack (data,attack_model, target_node_list,budget,features,adj,labels,test_model,verbose = False):
+def attack (data, attack_model, target_node_list, budget, features, adj, labels, test_model, verbose = False):
     miss = 0
     for target_node in tqdm(target_node_list):
         attack_model.attack(features, adj, labels, target_node, budget, verbose=verbose)
         modified_adj = attack_model.modified_adj
         if test_model == 'GCN':
-            acc = test_acc_GCN(modified_adj, features,data,
-                               target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+            acc = test_acc_GCN(modified_adj, features,data,  target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
         elif test_model == 'GIN':
-
-            acc = test_acc_GIN(modified_adj, features,data,
-                               target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+            acc = test_acc_GIN(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
         elif test_model == 'GSAGE':
-            acc = test_acc_GSAGE(modified_adj, features,data,
-                               target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+            acc = test_acc_GSAGE(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
         else:
             raise Exception("Test model is not supported")
         if acc == 0:
             miss += 1
     return miss / len(target_node_list)
+
+def attack_fga (data, attack_model, target_node_list, budget, features, adj, labels, test_model, idx_train, verbose = False):
+    miss = 0
+    for target_node in tqdm(target_node_list):
+        attack_model.attack(features, adj, labels, idx_train, target_node, budget, verbose=verbose)
+        modified_adj = attack_model.modified_adj
+        if test_model == 'GCN':
+            acc = test_acc_GCN(modified_adj, features,data,  target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        elif test_model == 'GIN':
+            acc = test_acc_GIN(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        elif test_model == 'GSAGE':
+            acc = test_acc_GSAGE(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        else:
+            raise Exception("Test model is not supported")
+        if acc == 0:
+            miss += 1
+    return miss / len(target_node_list)
+
+
+
+def attack_rnd (data, attack_model, target_node_list, budget, features, adj, labels, test_model, idx_train, verbose = False):
+    miss = 0
+    for target_node in tqdm(target_node_list):
+        attack_model.attack(adj, labels, idx_train, target_node, budget, verbose=verbose)
+        modified_adj = attack_model.modified_adj
+        if test_model == 'GCN':
+            acc = test_acc_GCN(modified_adj, features,data,  target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        elif test_model == 'GIN':
+            acc = test_acc_GIN(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        elif test_model == 'GSAGE':
+            acc = test_acc_GSAGE(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        else:
+            raise Exception("Test model is not supported")
+        if acc == 0:
+            miss += 1
+    return miss / len(target_node_list)
+
+
+def attack_ig(data, attack_model, target_node_list, budget, features, adj, labels, test_model, idx_train, verbose = False):
+    miss = 0
+    for target_node in tqdm(target_node_list):
+        attack_model.attack(features, adj, labels, idx_train, target_node, budget, steps=10, verbose=verbose)
+        modified_adj = attack_model.modified_adj
+        if test_model == 'GCN':
+            acc = test_acc_GCN(modified_adj, features,data,  target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        elif test_model == 'GIN':
+            acc = test_acc_GIN(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        elif test_model == 'GSAGE':
+            acc = test_acc_GSAGE(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        else:
+            raise Exception("Test model is not supported")
+        if acc == 0:
+            miss += 1
+    return miss / len(target_node_list)
+
+
+def attack_sga(data, attack_model, target_node_list, budget, features, adj, labels, test_model, verbose = False):
+    miss = 0
+    for target_node in tqdm(target_node_list):
+        attack_model.attack(features, adj, labels, target_node, budget, verbose=verbose)
+        modified_adj = attack_model.modified_adj
+        if test_model == 'GCN':
+            acc = test_acc_GCN(modified_adj, features,data,  target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        elif test_model == 'GIN':
+            acc = test_acc_GIN(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        elif test_model == 'GSAGE':
+            acc = test_acc_GSAGE(modified_adj, features,data, target_node)  # single_test(modified_adj, features, target_node, gcn=target_gcn)
+        else:
+            raise Exception("Test model is not supported")
+        if acc == 0:
+            miss += 1
+    return miss / len(target_node_list)
+
 
 ######################### Loading dataset  #########################
 data = Dataset(root='dataset', name=dataset_name)
@@ -142,13 +216,13 @@ adj, features, labels = data.adj, data.features, data.labels
 
 idx_train, idx_val, idx_test = data.idx_train, data.idx_val, data.idx_test
 
-idx_unlabeled = np.union1d(idx_val, idx_test)
+#idx_unlabeled = np.union1d(idx_val, idx_test)
 
 ######################### Setup GCN Surrogate model  #########################
 
 
 
-method = ['1518','Nettack']
+method = ['1518', 'Nettack', 'FGA', 'IGAttacks', 'SGAttacks', 'RND']
 budget_list = [1]
 rowlist = []
 test_model = 'GCN'
@@ -161,9 +235,9 @@ for budget in budget_list:
     surrogate = set_up_surrogate_model(features, adj, labels, idx_train, idx_val)
     model = OrbitAttack(surrogate, df_orbit, nnodes=adj.shape[0], device=device)
     model = model.to(device)
-
     miss_percentage = attack(data,model, target_node,budget,features,adj,labels,test_model)
     row.append(miss_percentage)
+
 
     # Nettack
     surrogate = set_up_surrogate_model(features, adj, labels, idx_train, idx_val)
@@ -173,7 +247,41 @@ for budget in budget_list:
     row.append(miss_percentage)
 
 
+    # FGA
+    surrogate = set_up_surrogate_model(features, adj, labels, idx_train, idx_val)
+    model = FGA(surrogate, nnodes=adj.shape[0], attack_structure=True, attack_features=False, device=device)
+    model = model.to(device)
+    miss_percentage = attack_fga(data, model, target_node, budget, features, adj, labels, test_model,idx_train)
+    row.append(miss_percentage)
+
+    # IGAttacks
+    surrogate = set_up_surrogate_model(features, adj, labels, idx_train, idx_val)
+    model = IGAttack(surrogate, nnodes=adj.shape[0], attack_structure=True, attack_features=False, device=device)
+    model = model.to(device)
+    miss_percentage = attack_ig(data, model, target_node, budget, features, adj, labels, test_model, idx_train)
+    row.append(miss_percentage)
+
+
+    # SGAttacks
+    surrogate = SGC(nfeat=features.shape[1], K=3, lr=0.1, nclass=labels.max().item() + 1, device=device)
+    surrogate = surrogate.to(device)
+    pyg_data = Dpr2Pyg(data)  # convert deeprobust dataset to pyg dataset
+    surrogate.fit(pyg_data, train_iters=200, patience=200, verbose=True)  # train with earlystopping
+    model = SGAttack(surrogate, attack_structure=True, device=device)
+    model = model.to(device)
+    miss_percentage = attack_sga(data, model, target_node, budget, features, adj, labels, test_model)
+    row.append(miss_percentage)
+
+    # RND
+    surrogate = set_up_surrogate_model(features, adj, labels, idx_train, idx_val)
+    model = RND()
+    model = model.to(device)
+    miss_percentage = attack_rnd(data, model, target_node, budget, features, adj, labels, test_model, idx_train)
+    row.append(miss_percentage)
+
+
     rowlist.append(row)
+
 
 result = pd.DataFrame(rowlist,columns=method,index=budget_list)
 result.to_csv('./results/result_GIN_no_feature.csv')
